f4de2b1ecb18c7d13e2c021b5b1c2733028ddfc6
PIBO0123009 Половинкин Семён Андреевич <PIBO0123009@sibupk.su>
2024-10-19 14:42:52 +0700

    Описания изменений

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите a:");
+            string input = Console.ReadLine();
+            a = int.Parse(input);
+            Console.WriteLine(a * a);
+        }
+    }
+}
